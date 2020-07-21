9e553523320008c5d8dea80d292a86f949d04f35
asherboy1 <70888116@qq.com>
2020-07-21 10:38:55 +0800

    firstday on git

---
+++ demo01.py
@@ -0,0 +1,16 @@
+# a = input("请输入"  )
+# print("input contend",a)
+
+# print(type(a))
+# print(type("哈哈"))
+# print(type(112))
+
+# a = float(input("输入a的值"))
+# b = float(input("输入b的值"))
+
+
+# print("he",a+b)
+
+a ='nvjfvkanvnaioenioenvoinvioenv'
+b = "sacaslxmlaks"
+print("he",int(len(a)+int(len(b))))
