830591d6aa0b177875dc73399eacbc07694314ec
asherboy1 <70888116@qq.com>
2020-07-30 11:08:59 +0800

    test5

--- demo02.py
+++ demo02.py
@@ -24,44 +24,44 @@
 # else:
 #     print("no")
 
-
+# ----------------------------------------
 # 成绩录入 5个 分开存放60
 
-# a = []
-# b = 0
-# high = {}
-# low = {}
-# e = 0
-# k = 0
-# while b < 5:
-#     a.append(input("名字:"))
-#     b = b + 1
+a = []
+b = 0
+high = {}
+low = {}
+e = 0
+k = 0
+while b < 5:
+    a.append(input("名字:"))
+    b = b + 1
 
-# print("所有学生", a)
+print("所有学生", a)
 
-# while e < len(a):
-#     k = int(input("输入"+a[e]+"成绩:"))
-#     if k > 60:
-#         high[a[e]] = k  # a[e]相当于key，k相当于value 添加入字典
-#     else:
-#         low[a[e]] = k
-#     e = e + 1
+while e < len(a):
+    k = int(input("输入"+a[e]+"成绩:"))
+    if k > 60:
+        high[a[e]] = k  # a[e]相当于key，k相当于value 添加入字典
+    else:
+        low[a[e]] = k
+    e = e + 1
 
-# print("及格", high)
-# print("不及格", low)
+print("及格", high)
+print("不及格", low)
+
+# -------------------------------------
+a = "cscasdasd"
+for i in a:
+    print(i)
+
+b = list(range(10))
+
+print(b)
 
 
-# a = "cscasdasd"
-# for i in a:
-#     print(i)
-
-# b = list(range(10))
-
-# print(b)
-
-
-# 9*9
-# for i in range(1, 10):
-#     for j in range(1, i+1):
-#         print(j, "*", i, "=", i*j, end="   ")
-#     print()  # 换行
+# 99乘法表--------------------
+for i in range(1, 10):
+    for j in range(1, i+1):
+        print(j, "*", i, "=", i*j, end="   ")
+    print()  # 换行
